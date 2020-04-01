9ffabe4ca0a8a1ed928dfd264050a83d3c83c0fc
Daryll  Osis <dosis@csumb.edu>
2020-04-01 12:52:58 -0700

    added GET and DELETE route for POSTS

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -49,7 +49,7 @@ router.post(
 
       if (!user) {
         return res
-          .status(400)
+          .status(404)
           .json({ msg: 'User not found, cannot make a post' });
       }
 
@@ -57,7 +57,7 @@ router.post(
       res.json(post);
     } catch (err) {
       console.error(err.message);
-      if (err.name == 'CastError') {
+      if (err.name === 'CastError') {
         return res
           .status(400)
           .json({ msg: 'User not found, cannot make a post' });
@@ -66,6 +66,64 @@ router.post(
     }
   }
 );
+
+// @route       GET api/posts/user/:user_id
+// @desc        Get all user's posts by user_id
+// @access      Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const posts = await Post.find({ user: req.params.user_id }).sort({
+      date: -1
+    });
+
+    // If there are no posts
+    if (posts.length < 1) {
+      return res.status(404).json({ msg: 'No posts for this user' });
+    }
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err);
+
+    if (err.name === 'CastError') {
+      res.status(400).json({ msg: 'No posts for this user' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route       DELETE api/posts/:id
+// @desc        Delete post by id (a user can only delete their own posts)
+// @access      Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // Check if the current logged in user is not the poster of the post
+    if (post.poster.user.toString() !== req.user.id) {
+      return res.status(401).json({
+        msg: 'User not authorized to delete'
+      });
+    }
+
+    // @TODO: delete
+    await post.remove();
+    res.json({ msg: 'Post is successfully deleted' });
+  } catch (err) {
+    console.error(err);
+
+    if (err.name === 'CastError') {
+      res.status(400).json({ msg: 'Post not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
 
 // @TODO: in action, check the parameters if it's someone's profile, if it is then just use the parameters for the post userid call. if it's empty(if a logged in user is on his page), call the posts post api with the REQ.USER.ID
 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -137,13 +137,13 @@ router.get('/user/:user_id', async (req, res) => {
     }).populate('user', ['firstName', 'lastName']);
 
     if (!profile) {
-      return res.status(400).json({ msg: 'Profile not found' });
+      return res.status(404).json({ msg: 'Profile not found' });
     }
 
     res.json(profile);
   } catch (err) {
     console.error(err.message);
-    if (err.name == 'CastError') {
+    if (err.name === 'CastError') {
       return res.status(400).json({ msg: 'Profile not found' });
     }
 
