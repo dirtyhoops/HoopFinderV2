e0b18d4def977cb2502431f8465b854a35e9e598
Daryll  Osis <dosis@csumb.edu>
2020-05-02 18:41:53 -0700

    fixsed flex for court

--- client/src/components/pages/LandingPage/FeaturedCourts.js
+++ client/src/components/pages/LandingPage/FeaturedCourts.js
@@ -119,6 +119,90 @@ const FeaturedCourts = () => {
               </span>
             </div>
           </div>
+
+          <div className='courts-grid-box'>
+            <div className='courts-grid-box__image'>
+              <img
+                src='https://www.courtsoftheworld.com/upload/courts/407/1/COTW_Henry-Schmidt-Park_1246825277.jpg'
+                className='courts-grid-box__image__img'
+              />
+            </div>
+            <div className='courts-grid-box__info'>
+              <p className='text-courtinfo-name'>McBride Loop Court</p>
+              <p className='text-courtinfo-location'>
+                922 McBride Loop, San Jose, CA 95125
+              </p>
+            </div>
+            <div className='courts-grid-box__footer__left'>
+              <span className='fa fa-star icon-star star-checked'></span>
+              <span className='fa fa-star icon-star star-checked'></span>
+              <span className='fa fa-star icon-star star-checked'></span>
+              <span className='fa fa-star icon-star star-checked'></span>
+              <span className='fa fa-star icon-star'></span>
+            </div>
+            <div className='courts-grid-box__footer__right'>
+              3{' '}
+              <span className='icon-user'>
+                <i className='fa fa-user'></i>
+              </span>
+            </div>
+          </div>
+
+          <div className='courts-grid-box'>
+            <div className='courts-grid-box__image'>
+              <img
+                src='https://www.courtsoftheworld.com/upload/courts/407/1/COTW_Henry-Schmidt-Park_1246825277.jpg'
+                className='courts-grid-box__image__img'
+              />
+            </div>
+            <div className='courts-grid-box__info'>
+              <p className='text-courtinfo-name'>McBride Loop Court</p>
+              <p className='text-courtinfo-location'>
+                922 McBride Loop, San Jose, CA 95125
+              </p>
+            </div>
+            <div className='courts-grid-box__footer__left'>
+              <span className='fa fa-star icon-star star-checked'></span>
+              <span className='fa fa-star icon-star star-checked'></span>
+              <span className='fa fa-star icon-star star-checked'></span>
+              <span className='fa fa-star icon-star star-checked'></span>
+              <span className='fa fa-star icon-star'></span>
+            </div>
+            <div className='courts-grid-box__footer__right'>
+              3{' '}
+              <span className='icon-user'>
+                <i className='fa fa-user'></i>
+              </span>
+            </div>
+          </div>
+
+          <div className='courts-grid-box'>
+            <div className='courts-grid-box__image'>
+              <img
+                src='https://www.courtsoftheworld.com/upload/courts/407/1/COTW_Henry-Schmidt-Park_1246825277.jpg'
+                className='courts-grid-box__image__img'
+              />
+            </div>
+            <div className='courts-grid-box__info'>
+              <p className='text-courtinfo-name'>McBride Loop Court</p>
+              <p className='text-courtinfo-location'>
+                922 McBride Loop, San Jose, CA 95125
+              </p>
+            </div>
+            <div className='courts-grid-box__footer__left'>
+              <span className='fa fa-star icon-star star-checked'></span>
+              <span className='fa fa-star icon-star star-checked'></span>
+              <span className='fa fa-star icon-star star-checked'></span>
+              <span className='fa fa-star icon-star star-checked'></span>
+              <span className='fa fa-star icon-star'></span>
+            </div>
+            <div className='courts-grid-box__footer__right'>
+              3{' '}
+              <span className='icon-user'>
+                <i className='fa fa-user'></i>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
