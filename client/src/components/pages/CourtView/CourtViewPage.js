4be02ed285297064001631a3aa17e4390893fe10
Daryll  Osis <dosis@csumb.edu>
2020-05-05 22:09:51 -0700

    started on courtview

--- client/src/components/pages/CourtView/CourtViewPage.js
+++ client/src/components/pages/CourtView/CourtViewPage.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 const CourtViewPage = () => {
   return (
-    <div>
-      <p>courts view page</p>
+    <div className='court-view-wrapper'>
+      <div className='court-view container'>
+        <div className='court-view-info'>
+          <p className='court-view-text-name u-text-uppercase'>rucker park</p>
+          <p className='court-view-text-address u-text-capitalize'>
+            155th street & frederick douglass blvd, new yord, NY 10039
+          </p>
+        </div>
+        <div className='court-view-properties'>
+          <p className='court-view-text-subheader'>court properties</p>
+          <div className='court-view-properties__grid'>
+            <div className='court-view-properties__grid-box'>surface type</div>
+            <div className='court-view-properties__grid-box'>surface type</div>
+            <div className='court-view-properties__grid-box'>surface type</div>
+            <div className='court-view-properties__grid-box'>surface type</div>
+            <div className='court-view-properties__grid-box'>surface type</div>
+            <div className='court-view-properties__grid-box'>surface type</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
