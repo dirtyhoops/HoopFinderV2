586c0017ce5ae1f6508ba04b9b9acc4ea6035f1d
Daryll  Osis <dosis@csumb.edu>
2020-05-21 20:05:27 -0700

    polished the recent reviews a little bit

--- client/src/components/pages/WriteaReviewPage/RecentReviews.js
+++ client/src/components/pages/WriteaReviewPage/RecentReviews.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import Moment from 'react-moment';
 
-import iconstar_sm from '../../../img/icon-star-sm.png';
-import iconstar_lg from '../../../img/icon-star-lg.png';
-
 const RecentReviews = (props) => {
   const { reviews } = props;
   const [toggler, setToggler] = useState(true);
@@ -24,48 +21,60 @@ const RecentReviews = (props) => {
         className='writeareview-recentreviews-toggler'
         onClick={() => toggleHandler()}
       ></div>
-      <div className='writeareview-recentreviews-header'>
-        <p>Recent Reviews</p>
-      </div>
-      <div className='writeareview-recentreviews-grid'>
-        {reviews.map((review) => (
-          <div key={review._id} className='writeareview-recentreviews-grid-box'>
-            <div className='writeareview-recentreviews-grid-box__header'>
-              <div className='writeareview-recentreviews-grid-box__header__image'>
-                <img
-                  src={review.avatar}
-                  className={`writeareview-recentreviews-grid-box__header__image__img ${review.avatar_bg}`}
-                />
+      <div className='writeareview-recentreviews-content'>
+        <div className='writeareview-recentreviews-header'>
+          <p>Recent Reviews</p>
+        </div>
+        <div className='writeareview-recentreviews-grid'>
+          {reviews.map((review) => (
+            <div
+              key={review._id}
+              className='writeareview-recentreviews-grid-box'
+            >
+              <div className='writeareview-recentreviews-grid-box__header'>
+                <div className='writeareview-recentreviews-grid-box__header__image'>
+                  <img
+                    src={review.avatar}
+                    className={`writeareview-recentreviews-grid-box__header__image__img ${review.avatar_bg}`}
+                  />
+                </div>
+                <div className='writeareview-recentreviews-grid-box__header__info'>
+                  <p className='writeareview-recentreviews-text-name'>
+                    {review.firstName} {review.lastName}
+                  </p>
+                  <p className='writeareview-recentreviews-text-place'>
+                    {review.city}, {review.state}
+                  </p>
+                </div>
               </div>
-              <div className='writeareview-recentreviews-grid-box__header__info'>
-                <p className='writeareview-recentreviews-text-name'>
-                  {review.firstName} {review.lastName}
+              <div className='writeareview-recentreviews-grid-box__rating'>
+                <div
+                  className='writeareview-recentreviews-grid-box__rating__stars'
+                  style={{
+                    '--font-size': '13px',
+                  }}
+                >
+                  {/* {review.rating} */}
+                  <div className={`rating rating-${review.rating}`}>
+                    <i className='star-1'>★</i>
+                    <i className='star-2'>★</i>
+                    <i className='star-3'>★</i>
+                    <i className='star-4'>★</i>
+                    <i className='star-5'>★</i>
+                  </div>
+                </div>
+                <div className='writeareview-recentreviews-grid-box__rating__date'>
+                  <Moment format='M/D/YYYYY'>{review.dateCreated}</Moment>
+                </div>
+              </div>
+              <div className='writeareview-recentreviews-grid-box__text'>
+                <p className='writeareview-recentreviews-text-review'>
+                  {review.text}
                 </p>
-                <p className='writeareview-recentreviews-text-place'>
-                  {review.city}, {review.state}
-                </p>
               </div>
             </div>
-            <div className='writeareview-recentreviews-grid-box__rating'>
-              <div className='writeareview-recentreviews-grid-box__rating__stars'>
-                {/* {review.rating} */}
-                <img src={iconstar_sm} />
-                <img src={iconstar_sm} />
-                <img src={iconstar_sm} />
-                <img src={iconstar_sm} />
-                <img src={iconstar_sm} />
-              </div>
-              <div className='writeareview-recentreviews-grid-box__rating__date'>
-                <Moment format='M/D/YYYYY'>{review.dateCreated}</Moment>
-              </div>
-            </div>
-            <div className='writeareview-recentreviews-grid-box__text'>
-              <p className='writeareview-recentreviews-text-review'>
-                {review.text}
-              </p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
